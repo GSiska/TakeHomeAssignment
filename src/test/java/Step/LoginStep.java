8654067fabbb2fc600744398ed4d52c155b8a583
Siska <siska@fazzfinancial.com>
2023-06-03 13:34:07 +0700

    optimize LoginStep

--- src/test/java/Step/LoginStep.java
+++ src/test/java/Step/LoginStep.java
@@ -9,32 +9,25 @@ import org.openqa.selenium.WebDriver;
 
 public class LoginStep {
 
-        private WebDriver driver;
-        private WebDriverManager webDriverManager;
-
-        public LoginStep() {
-            webDriverManager = new WebDriverManager();
-            driver = webDriverManager.getDriver();
-        }
+        WebDriverManager webDriverManager = new WebDriverManager();
+        private WebDriver driver = webDriverManager.getDriver();
+        LoginPage loginPage = new LoginPage(driver);
+        HomePage homePage = new HomePage(driver);
 
         public void userIsAtLoginPage() {
-            LoginPage loginPage = new LoginPage(driver);
             driver.get(loginPage.websiteLink);
         }
 
         public void userInputsValidUsernameAndPassword() {
-            LoginPage loginPage = new LoginPage(driver);
             loginPage.inputLoginUserName();
             loginPage.inputloginPassword();
         }
 
         public void userClicksLoginButton() {
-            LoginPage loginPage = new LoginPage(driver);
             loginPage.loginSubmitButton.click();
         }
 
         public void userIsAtHomepage() {
-            HomePage homePage = new HomePage(driver);
             Assert.isTrue(homePage.homepageLogoIsDisplayed(),"Homepage not found!");
         }
 }
