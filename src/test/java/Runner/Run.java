adbd91bd5f5646e52efac302c1a8146be99262f4
Siska <siska@fazzfinancial.com>
2023-06-07 16:31:01 +0700

    update file Run to add cucumber report

--- src/test/java/Runner/Run.java
+++ src/test/java/Runner/Run.java
@@ -4,8 +4,9 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
-        features = "src/test/userCredential/login.feature",
-        glue = {"StepDefinition"}
+        features = "src/test/Transaction/ProceedTransaction.feature",
+        glue = {"StepDefinition"},
+        plugin = {"pretty","html:target/cucumber-reports.html"}
 )
 public class Run extends AbstractTestNGCucumberTests {
 }
